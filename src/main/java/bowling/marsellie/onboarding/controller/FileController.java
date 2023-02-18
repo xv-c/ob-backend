88753645655e204dc9058b4c9673723b346555a2
m.maksiutov <m.maksiutov@bars.group>
2023-02-18 16:00:47 +0300

    added TestService

--- src/main/java/bowling/marsellie/onboarding/controller/FileController.java
+++ src/main/java/bowling/marsellie/onboarding/controller/FileController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(Endpoints.FILE)
-@PreAuthorize("isAuthenticated()")
 @RequiredArgsConstructor
 public class FileController {
     private final FileService fileService;
